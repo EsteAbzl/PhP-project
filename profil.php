07fe7b92d0cc94f6e96664f31ce72a51ff865854
EsteAbzl <esteabzl@gmail.com>
2024-05-14 15:59:30 +0200

    ajout de la page profil

---
+++ profil.php
@@ -0,0 +1,15 @@
+<?php
+    include 'scripts/check_session.php';
+    include 'template.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<div class="main">
+    test test
+    <p>test</p>
+
+</div>
+
+</html>
