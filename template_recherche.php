522636f650564bd221ed6b0981d63412111ea905
EsteAbzl <esteabzl@gmail.com>
2024-05-15 00:17:11 +0200

    ajustement template recherche

--- template_recherche.php
+++ template_recherche.php
@@ -10,7 +10,9 @@
         }
 
         .main { 
-            top: 20vw;
+            position: absolute;
+            top: 20vh;
+            left: 22vw;
         }
 
         header {
@@ -20,12 +22,11 @@
                 width: 100vw;
                 height: 18vh;
                 z-index: 500;
-            box-shadow: 5vh 0 4vh 0.5vh rgba(0, 0, 0, 0.6);
-            
+                
+            padding: 0;
             color: black;
             background: linear-gradient(#e66465, #9198e5);
-            
-            text-align: center;
+            box-shadow: 0vh 0 4vh 0.5vh rgba(9, 22, 36, 0.6);
         }
 
         #search-box {
@@ -35,32 +36,36 @@
                 width: 30vw;
             padding: 0.5vh 1vh 0.5vh;
 
-            box-shadow: 0 0 3vh 0.5vh rgba(0, 0, 0, 0.5);
+            box-shadow: 0 0 3vh 1vh rgba(227, 227, 227, 0.664);
             border-radius: 2vh;
+            border-style: hidden;
                         
             font-family: 'quicksand';
             font-size: 3vh;
         }
 
-        #search-btn {
+        button {
             position: absolute;
                 top: 3vh;
                 right: 20vw;
                 width: 10vh;
                 height: 10vh;
             
-                box-shadow: 0 0 1vh  rgba(0, 0, 0, 0.3);
+            box-shadow: 0 0 0.5vh 0.1vh rgba(180, 180, 180, 0.155);
             border-radius: 2vh;
+            border-style: hidden;
             
             color: black;
-            background-color: rgba(105, 105, 105, 0.226);
+            background-color: rgba(12, 18, 52, 0.14);
             transition: background-color 0.3s ease;
             
             cursor: pointer;
+            font-size: 0;
         }
 
-        #search-btn:hover {
-            background-color: #B0B0B029; /* Couleur de fond au survol */
+        button:hover {
+            background-color: #85858500; /* Couleur de fond au survol */
+            box-shadow: 0 0 0 0 rgba(180, 180, 180, 0);
         }
 
         .icone {
@@ -68,6 +73,7 @@
             top: 0.5vh;
             left: 0.5vh;
 
+            border-style: hidden;
             width: 6vh;
             height: 6vh;
             opacity: 0.6;
@@ -79,7 +85,7 @@
 <header>
     <form action="process_rechercher.php" method="get">
         <input type="text" id="search-box" name="q" placeholder="Chercher">
-        <button type="submit" id="search-btn"><img src="./icones/026-search.png" alt="Icône" class="icone"></button>
+        <button type="submit"><img src="./icones/026-search.png" alt="Icône" class="icone"></button>
 
     </form>
 </header>
