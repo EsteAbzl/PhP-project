21d43704e63245fcb3191f082c27337ef5414e00
PaulHPSR <onekcrat@gmail.com>
2024-05-16 21:39:56 +0200

    Mise en place possibilité de changer de pfp

--- scripts/modif_profil.php
+++ scripts/modif_profil.php
@@ -1,9 +1,15 @@
 <?php
 include 'check_session.php';
 
+
+
+
 $profil_dir = "../data/profils/" . $_SESSION['pseudo'];
 $profil_csv = $profil_dir . "/profil.csv";
 $bio_csv = $profil_dir . "/bio.csv"; 
+$pfp_path = $profil_dir . "/pfp.jpg"; 
+
+
 
 $new_pseudo = $_POST['pseudo'];
 $new_motdepasse = $_POST['motdepasse'];
@@ -12,9 +18,14 @@ $new_prenom = $_POST['prenom'];
 $new_datenaissance = $_POST['datenaissance'];
 $new_email = $_POST['email'];
 $new_genre = $_POST['genre'];
-$new_biographie = $_POST['biographie']; 
+$new_biographie = $_POST['biographie'];
+
+
 
 $ligne2 = file($profil_csv)[1];
+
+
+
 
 $new_data = "ELEMENT;VALEUR;\n";
 $new_data .= $ligne2; // Ajout de la ligne 2 en mémoire
@@ -32,22 +43,58 @@ unlink($profil_csv);
 file_put_contents($profil_csv, $new_data);
 
 
+
+
 if (file_exists($bio_csv)) {
 
     unlink($bio_csv);
 
 }
 
+// Créer le nouveau fichier bio.csv avec la nouvelle biographie
 file_put_contents($bio_csv, $new_biographie);
+
+
+if (file_exists($pfp_path)) {
+
+    unlink($pfp_path);
+
+}
+
+// Renommer et déplacer la nouvelle photo de profil
+if ($_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+
+    $temp_file = $_FILES['photo']['tmp_name'];
+    $new_pfp_path = $profil_dir . "/pfp.jpg";
+
+
+
+    if (move_uploaded_file($temp_file, $new_pfp_path)) {
+        echo "La photo de profil a été mise à jour avec succès.";
+
+    } else {
+
+        echo "Une erreur est survenue lors du téléchargement de la nouvelle photo de profil.";
+    }
+} else {
+    
+    echo "Une erreur est survenue lors de la réception de la nouvelle photo de profil.";
+}
 
 $nouveauChemin = "../data/profils/" . $new_pseudo;
 
+
+
 if (rename($profil_dir, $nouveauChemin)) {
+
+
+
     $_SESSION['pseudo'] = $new_pseudo;
-    echo "<script>location.href=\"../my_profil.php\";</script>"; 
+
+    echo "<script>location.href=\"../my_profil.php\";</script>"; // Redirection vers le profil
+
     exit();
 } else {
     echo "Une erreur est survenue lors du renommage du dossier.";
 }
-
 ?>
