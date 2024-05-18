c38e90fd78894c9da5e2104c1e78be02ab1853dd
PaulHPSR <onekcrat@gmail.com>
2024-05-18 15:35:30 +0200

    petit changement -> include add_notif_2 au lieu de l'avoir dans le code

--- sabo.php
+++ sabo.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'scripts/check_session.php';
+include 'add_notifs_2.php'
 
 $pseudo_recevant = isset($_GET['pseudo']) ? $_GET['pseudo'] : '';
 
@@ -89,23 +90,6 @@ function abo($pseudo_recevant) {
 
 }
 
-
-//Notification d'abonnement
-function addNotification2($pseudo_recevant) {
-
-    $file = 'data/profils/'. $pseudo_recevant .'/notifs.csv';
-    $data = '2;' . $_SESSION['pseudo'] . ';' . date("Y-m-d H:i:s") . ';' . $_SESSION['pseudo'] . ' vous suit.' . "\n";
-
-    $handle = fopen($file, 'a');
-    if ($handle) {
-
-        fwrite($handle, $data);
-        fclose($handle);
-
-    } else {
-        echo "Impossible d'ouvrir le fichier pour écriture.";
-    }
-}
 
 
 abo($pseudo_recevant);
