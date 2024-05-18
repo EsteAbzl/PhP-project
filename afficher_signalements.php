cf963e209d81618c26b06a20eef051cf17956b51
PaulHPSR <onekcrat@gmail.com>
2024-05-18 16:33:27 +0200

    Début de Template_admin + Signalement des messages ok

---
+++ afficher_signalements.php
@@ -0,0 +1,61 @@
+<?php
+include 'scripts/check_session.php';
+include 'template_admin.php';
+
+function ListeNotif() {
+    $resultats = array();
+    $chemin_fichier = "data/signalements.csv";
+
+    if (($dossier = fopen($chemin_fichier, "r")) !== FALSE) {
+
+        while (($data = fgetcsv($dossier, 1000, ";")) !== FALSE) {
+            $ps = $data[0];
+            $resultats[] = array(
+                'pseudo' => $data[0],
+                'lien_photo' => "data/profils/" . $ps . "/pfp.jpg",
+                'date' => $data[2], 
+                'message' => $data[1],
+                 'id' => $data[3]
+            );
+        }
+
+        fclose($dossier);
+    } else {
+
+        echo "Erreur lors de l'ouverture du fichier.";
+    }
+
+    return $resultats;
+}
+
+$resultats = ListeNotif();
+
+
+$hauteurListe = count($resultats) * 120; 
+
+if (!empty($resultats)) {
+    echo '<div style="text-align: center; width: 24vw; height: ' . $hauteurListe . 'px; overflow-y: hidden; margin: 0 auto;">'; 
+    echo '<ul style="list-style: none; padding: 0;">'; 
+    foreach ($resultats as $resultat) {
+        echo '<li style="margin-bottom: 10px; border: 2px solid lightgrey; padding: 10px;">'; 
+        echo '<button style="border: none; background: none; display: flex; align-items: center;">';
+        echo '<img src="' . $resultat['lien_photo'] . '" alt="Photo de profil" style="border-radius: 50%; width: 50px; height: 50px; margin-right: 10px;">';
+        echo '<div style="display: flex; flex-direction: column;">';
+        echo '<span style="font-weight: bold; font-size: 18px; font-family: \'Quicksand\', sans-serif;">' . $resultat['id'] . '</span>';
+        echo '<span style="font-weight: bold; font-size: 18px; font-family: \'Quicksand\', sans-serif;">' . $resultat['message'] . '</span>';
+        echo '<span style="font-size: 14px; color: grey;">' . $resultat['date'] . '</span>'; // Ajout de la date en dessous du message
+        echo '</div>';
+        echo '</button>';
+        echo '</li>';
+    }
+    echo '</ul>';
+    echo '</div>'; 
+} else {
+    echo 'Aucun résultat trouvé.';
+}
+?>
+
+<!-- Bouton "Effacer" aligné à gauche -->
+<form id="deleteNotifsForm" action="scripts/delete_signalements.php" method="post" style="text-align: right; margin-left: 10px;">
+    <input type="submit" name="deleteNotifs" value="Effacer" style="border: 2px solid red; background-color: white; color: red; padding: 10px; font-size: 16px; cursor: pointer;">
+</form>
---
+++ template_admin.php
@@ -0,0 +1,169 @@
+<!-- 
+    Page copié au début de pages
+    Contient les liens vers les différentes pages du site 
+-->
+
+
+<!DOCTYPE html>
+<head>
+    <title>template</title>
+    <style>
+        
+        @font-face {
+            font-family: 'against.projet';
+            src: url('./fonts/against regular.otf') format('opentype'); 
+        }
+
+        @font-face {
+            font-family: 'quicksand';
+            src: url('./fonts/Quicksand_Light.otf') format('opentype');
+        }
+        
+        body{
+            background-color: white;
+        }
+
+        /*pour ecrire dans la page*/
+        .main { 
+            margin-left: 25vw;
+        }
+
+        /*permet de se déplacer dans le site*/
+        .sommaire {
+            position: fixed;
+                top: 0;
+                left: 0;
+                width: 23vw;
+                height: 100vh;
+                z-index: 999;
+
+            padding: 0;
+            background-color: white;
+            box-shadow: 0 0 1vh rgba(0, 0, 0, 0.3);
+        }
+
+
+        .logo {
+            margin-left: 15%;
+            box-sizing: border-box;
+            
+            color: black;                 /* Couleur du bouton à modifier en fonction de DA */
+            background-color: none;
+            transition: background-color 0.3s ease;
+            
+            font-family: 'against.projet';
+            font-size: 3vw;
+            text-align: left;
+            text-decoration: none;
+        }
+
+
+
+
+        .profil {
+            margin-top: 0.3vw;
+            margin-left: 18%;
+            box-sizing: border-box;
+            width: 80%;
+            
+            color: black;                 /* Couleur du bouton à modifier en fonction de DA */
+            background-color: transparent;
+            cursor: pointer;
+            
+            font-family: 'against.projet';
+            font-size: 2vw;
+            text-align: left;
+            text-decoration: none;
+        }
+
+        .pfp {
+            position: relative;
+            top: 0.5vw;
+            left: 0vw;
+            width: 2.5vw;
+            height: 2.5vw;
+            margin-right: 1vw;
+
+            border: 0.25vw solid rgba(102, 64, 150, 0.424);
+            border-radius: 2vw;
+
+            opacity: 1;
+        }
+
+
+
+
+        .boutons {
+            display: flex;
+            flex-direction: column;
+            margin-top: 1vw;
+        }
+
+        .bouton {
+            width: 70%;
+            margin-left: 15%;
+            margin-top: 1vh;
+            padding: 2% 0 4% 4%;
+            border-radius: 1.25vw;
+            box-shadow: 0 0 0.8vh rgba(0, 0, 0, 0.3);
+            box-sizing: border-box;
+            
+            color: black;                 /* Couleur du bouton à modifier en fonction de DA */
+            background-color: #F4F3F3;
+            transition: background-color 0.3s ease;
+            
+            font-family: 'quicksand';
+            font-size-adjust: initial;
+            font-size: 1.5vw;
+            text-decoration: none;
+            text-align: left;
+        }
+
+        .bouton:hover {
+            background-color: #ffffff29; /* Couleur de fond au survol */
+        }
+
+        .image {
+            position: relative;
+            top: 0.5vh;
+            margin-right: 1vw;
+
+            width: 2.5vw;
+            height: 2.5vw;
+            opacity: 0.6;
+        }
+
+    </style>
+</head>
+
+
+
+<body>
+
+    
+    <div class="sommaire">
+        <div class="logo">
+            <img src="./icones/046-envato.png" alt="Icône" class="image">Dater
+        </div>
+
+        <script>
+            function link_profil(){
+                location.href="./show_profil.php?pseudo=<?php echo $_SESSION['pseudo']; ?>";
+            }
+        </script>
+
+        <div class="profil" onclick="link_profil()">
+            <img src=<?php echo '"data/profils/'.$_SESSION['pseudo'].'/pfp.jpg"'; ?> alt="Icône" class="pfp"><?php echo $_SESSION['pseudo']; ?>
+        </div>
+
+        <div class="boutons">
+            <a href="" class="bouton"><img src="./icones/home.png" alt="Icône" class="image"></a>
+            <a href="#" class="bouton"><img src="./icones/026-search.png" alt="Icône" class="image">Rechercher une discussion</a>
+            <a href="afficher_signalements.php" class="bouton"><img src="./icones/043-warning.png" alt="Icône" class="image">Signalements</a>
+            <a href="#" class="bouton"><img src="./icones/profil.png" alt="Icône" class="image">Blacklisted</a>
+        </div>
+
+    </div>
+    
+
+</body>
