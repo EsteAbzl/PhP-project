9a15758499fa1596692693c7dfa1656338ef46c1
EsteAbzl <esteabzl@gmail.com>
2024-05-21 16:00:18 +0200

    Bannir utilisateur

--- admin/admin_ban.php
+++ admin/admin_ban.php
@@ -13,35 +13,43 @@
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            height: 100vh;
             margin: 0;
+            font-size: 2vw;
         }
 
         form {
-            width: 300px;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
+            width: 30vw;
+            padding: 1vw;
+            border: 0.1vw solid #ccc;
+            border-radius: 0.5vw;
             background-color: #f9f9f9;
         }
 
-        input[type=text] {
+        input[type=search] {
             width: 100%;
-            padding: 10px;
-            margin: 6px 0;
+            padding: 0.5vw;
+            margin: 1vw 0;
             box-sizing: border-box;
-            border: 1px solid #ccc;
-            border-radius: 4px;
+            
+            border: 0.1vw solid #ccc;
+            border-radius: 0.5vw;
+            outline: transparent;
+
+            font-size: 1.5vw;
         }
 
         input[type=submit] {
             width: 100%;
+            padding: 0.7vw;
+            margin: 0.5vw 0;
+            
             background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            margin: 8px 0;
             border: none;
-            border-radius: 4px;
+            border-radius: 0.4vw;
+
+            color: white;
+            font-size: 1.5vw;
+
             cursor: pointer;
         }
 
@@ -54,7 +62,24 @@
     <h2>Bannir un utilisateur</h2>
     
     <form action="../scripts/process_ban.php" method="post">
-        Pseudo: <input type="text" name="pseudo"><br>
+        Pseudo:<br>
+        <input type="search" list="pseudo" name="pseudo" required>
+        <datalist id="pseudo">
+            <?php
+                $file = fopen("../data/profil_list.csv", "r");
+                while(!feof($file)){
+                    $line = fgetcsv($file, 20, "]");
+                    if(isset($line) && $line[0]!=""){
+                        $pseudo =  $string = substr($line[0], 1);
+                        if($pseudo != $_SESSION['pseudo']){
+                            echo '<option value="'.$pseudo.'">';
+                        }
+                    }
+                }
+                fclose($file);
+            ?>
+        </datalist><br>
+
         <input type="submit" value="Bannir">
     </form>
 </body>
