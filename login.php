f1a1d0e9efabbe9cc6863a7b967302e94f08b6cc
EsteAbzl <esteabzl@gmail.com>
2024-05-22 04:16:14 +0200

    style de l'accueil

--- login.php
+++ login.php
@@ -15,31 +15,36 @@
             }
 
             .logo {
-                font-family: 'against.projet';
-                font-size: 70px;
-                color: rgba(255, 255, 255, 5);                 
-                text-decoration: none;
-                width: 400px;
-                text-align: center;
-                margin-left: 30px;
                 position: absolute;
-                top: 35px;
-                left: 20px;
+                top: 3vw;
+                left: 0vw;
+                width: 40vw;
+
+                font-family: 'against.projet';
+                color: rgba(255, 255, 255, 1);                 
+                font-size: 7vw;
+                text-decoration: none;
+                text-align: center;
             }
 
             .image {
-                width: 50px;
-                height: 50px;
-                margin-right: 10px;
+                position: relative;
+                top: 2vw;
+                left: 0vw;
+                width: 6vw;
+                height: 6vw;
+                margin-right: 1vw;
             }
 
             body{
+                overflow: hidden;
                 margin: 0;
                 padding:0;
                 background-image: url('https://tinder.com/static/build/184abbc6dbfb37e127d91e1695d0a468.webp');
                 background-size: cover;
                 background-position: center;
                 height: 100vh;
+                width: 100vw;
             }
 
             .overlay{
@@ -55,17 +60,22 @@
 
 
             .bouton {
-                font-size: 30px; 
-                background-color: #59EE59; 
-                color: white; 
+                position: relative;
+                right: 2vw;
+                margin-left: 5vw;
+                padding: 2vw;
+
+                background-color: #69aE69; 
                 text-decoration : none;
-                padding: 30px 40px;
-                border-radius: 20px; 
+                
+                box-shadow: 0 0.5vw 1vw rgba(0, 0, 0, 0.65);
                 border: none; 
-                font-weight: bold; 
+                border-radius: 1vw; 
                 transition: background-color 0.3s ease; 
-                margin-left: 100px;
-                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.65);
+                
+                color: white; 
+                font-size: 2vw; 
+                font-weight: bold; 
             }
           
             .bouton:hover {
@@ -74,14 +84,58 @@
 
             .container {
                 position: absolute;
-                top: 200px;;
-                left: 700px;;
-                width: 900px;
-                height: 700px;
+                top: 2vw;
+                left: 45vw;
+                width: 40vw;
+                height: 15vw;
                 background-color: rgba(0, 0, 0, 0.7);
-                border-radius: 20px; 
-                padding: 30px;
-                box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); 
+                border-radius: 3vw; 
+                padding: 2vw;
+                box-shadow: 0 0 3vw rgba(0, 0, 0, 0.5); 
+
+                text-align: left;
+            }
+
+            h1 {
+                margin-bottom: 2vw; 
+                margin-left: 5vw;
+                box-shadow: 0 0 2vw rgba(0, 0, 0, 0.5); 
+                
+                
+                color: white; 
+                font-family:'against.project'; 
+                font-size: 2vw; 
+                font-weight: bold:
+            }
+
+            form {
+                font-size: 0;
+            }
+
+            label {
+                margin-left:2vw;
+                
+
+                color: white; 
+                font-family:'quicksand';
+                font-size: 1.5vw; 
+            }
+
+            input {
+                height: 2vw; 
+                width: 27vw; 
+                margin-top: 0.5vw; 
+                margin-bottom: 0.7vw;
+                margin-left:4vw;
+
+                border-style: hidden;
+                
+                padding: 0;
+                padding-left: 1vw;
+
+
+                border-radius: 1vw;
+                font-size: 1vw; 
             }
         </style>
 
@@ -95,22 +149,22 @@
             </div>
             <div class="container">
                 
-                <h1 style="color: white; font-family:'against.project'; font-size: 55px; margin-bottom: 30px; margin-left: 300px;box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);  ">Connexion</h1>
+                <h1 >Connexion</h1>
 
                 
                 <form id="login" action="./scripts/process_login.php" method="POST">
                     
-                    <label style="color: white; font-size: 30px; font-family:'quicksand';margin-top: 100px; margin-left:330px;" for="pseudo">Pseudo :</label><br>
-                    <input style="height: 35px; font-size: 25px; width: 700px; margin-left:100px; margin-top: 20px; border-radius: 15px; " type="text" id="pseudo" name="pseudo" maxlength="15" required><br>
+                    <label for="pseudo">Pseudo :</label><br>
+                    <input autofocus type="text" id="pseudo" name="pseudo" maxlength="15" required><br>
 
-                    <label style="color: white; font-size: 30px; font-family:'quicksand';margin-top: 300px; margin-left: 330px;" for="motdepasse">Mot de passe :</label><br>
-                    <input style="height: 35px; font-size: 25px; width: 700px; margin-left:100px; margin-top: 20px; border-radius: 15px; " type="password" id="motdepasse" name="motdepasse" required><br>
+                    <label  for="motdepasse">Mot de passe :</label><br>
+                    <input  type="password" id="motdepasse" name="motdepasse" required><br>
 
 
-                    <input style="margin-left: 35%; margin-top: 200px;" type="submit" value="Se connecter" class="bouton">
+                    <input style="margin-left: 5vw; margin-top: 3vw; height: 6vw" type="submit" value="Se connecter" class="bouton">
                 </form>
             </div>
         </div>
-        <p style="font-family: 'quicksand'; font-size: 14px; margin-left:30px; color: white;"><strong>© Site entièrement codé par : Esteban Abehzele, Paul Hopsore, Ilan Dassonville, Zachary Weiss.</strong></p>
+        <p style="font-family: 'quicksand'; font-size: 1vw; color: white;"><strong>© Site entièrement codé par : Esteban Abehzele, Paul Hopsore, Ilan Dassonville, Zachary Weiss.</strong></p>
     </body>
 </html>
